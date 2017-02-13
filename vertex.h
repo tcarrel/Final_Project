cad814cb6a5c283f597520a87a2b1c3c20584d19
Thomas Russel Carrel <tcarrel@mail.csuchico.edu>
2017-02-13 11:51:28 -0800

    Create struct to store per-vertex data.

---
+++ vertex.h
@@ -0,0 +1,27 @@
+/**
+ * \file vertex.h
+ * \author Thomas R. Carrel
+ */
+
+
+#ifndef  VERTEX_H
+# define VERTEX_H
+
+#include<initializer_list>
+
+#include<GL/gl.h>
+
+/** Struct for storing vertex coordinates.
+ */
+struct Vertex
+{
+    GLfloat x; ///< x-coordinate.
+    GLfloat y; ///< y-coordinate.
+    GLfloat z; ///< z-coordinate.
+/*
+    GLfloat u; ///< Texture coordinate, u.
+    GLfloat v; ///< Texture coordinate, v
+*/
+};
+
+#endif /* VERTEX_H */
