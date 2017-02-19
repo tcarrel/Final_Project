cc4eec931440961f2f41760fffd38d572e51640e
Thomas Russel Carrel <tcarrel@mail.csuchico.edu>
2017-02-18 16:05:34 -0800

    Created the basic files for the scene graph.

---
+++ model/scene_graph.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "scene_graph.h"
+#include "model.h"
---
+++ model/scene_graph.h
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+#ifndef  _SCENE_GRAPH_H_
+# define _SCENE_GRAPH_H
+
+class Model;
+
+class Scene_Graph
+{
+    public:
+
+    private:
+        Model* world_; ///< The root of the scene graph.
+};
+
+#endif
