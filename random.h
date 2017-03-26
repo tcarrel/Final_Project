8214a91e0be915face2037d9cfde05068c9e6ef6
Thomas Russel Carrel <tcarrel@mail.csuchico.edu>
2017-03-26 10:17:37 -0700

    Changed the Random to allow it to work as a template in order to return varying types.

--- random.h
+++ random.h
@@ -16,14 +16,97 @@
 #ifndef  _RANDOM_H_
 # define _RANDOM_H_
 
+
 /** Encapsulates random number generation.
- */
+*/
 class Random
 {
     public:
-        static uint32_t rand_unsigned( uint32_t );
+        /** Seeds the random number generator only once.
+        */
+        void seed( void )
+        {
+            if( !seeded )
+            {
+                seeded = true;
+                srand( time( NULL ) );
+            }
+        }
     private:
         static bool seeded;
 };
 
+
+
+/** Tracks whether or not the random number generator has been seeded.
+ */
+bool Random::seeded = false;
+
+
+
+
+
+/**
+ * @return A random number [0,1].
+ */
+template<typename T> T get_rand( void )
+{
+    Random r;
+    r.seed();
+
+    return static_cast<T>(rand()) / static_cast<T>(RAND_MAX);
+}
+
+
+
+
+
+
+/**
+ *@param max the top end of the range to be returned.
+ * @return Random number [0, max).
+ */
+template<typename T> T get_rand( T max )
+{
+    Random r;
+    r.seed();
+
+    return static_cast<T>(rand()) / (static_cast<T>(RAND_MAX/max));
+}
+
+
+
+
+
+/**
+ * @param min The minimum of the range.
+ * @param max The maximum of the range.
+ * @return Random number in the range [min,max].
+ */
+template<typename T> T get_rand( T min, T max )
+{
+    Random r;
+    r.seed();
+    T num = static_cast<T>(rand());
+    T den = static_cast<T>(RAND_MAX/(max - min));
+
+    return min + (num/den);
+}
+
+
+
+/**
+ * @param min The minimum of the range.
+ * @param max The maximum of the range.
+ * @return Random number in the range [min,max].
+ */
+template<typename T, typename U> T get_rand( T min, U max )
+{
+    Random r;
+    r.seed();
+    T num = static_cast<T>(rand());
+    T den = static_cast<T>(RAND_MAX/(max - min));
+
+    return min + (num/den);
+}
 #endif
