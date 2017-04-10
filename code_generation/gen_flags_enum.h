c118b58d585de1ac4d925ea7b04e2878833746d6
Thomas Russel Carrel <tcarrel@mail.csuchico.edu>
2017-04-10 12:54:19 -0700

    Cleaned up a file that I'm not going to be using any time soon...

--- code_generation/gen_flags_enum.h
+++ code_generation/gen_flags_enum.h
@@ -12,34 +12,56 @@
  * can be easily combined when passed into functions.
  */
 
-/** Flags for generating Computed shaders.
+/** Underlying type for Computer shader options enum.
  */
 typedef unsigned long long int Com_gen_enum;
+/** Flags for generating Computed shaders.
+ */
+# define Com_gen_enum_SIZE          (sizeof(Com_gen_enum))
+# define COM_EMPTY_SHADER           ((Com_gen_enum) 0)
 
-/** Flags for generating Vertex shaders.
+/** Underlying type for Vertex shader options enum.
  */
 typedef unsigned long long int Ver_gen_enum;
-#  define Vert_gen_enum_SIZE (sizeof(Vert_gen_enum))
-#  define EMPTY_SHADER           ((Vert_gen_enum) 0)
-#  define VERT_POSITION          ((Vert_gen_enum) 1)
-#  define VERT_VERTEX_COLORING   ((Vert_gen_enum) 1<<1)
-#  define VERT_VIEW_POSITION_UNI ((Vert_gen_enum) 1<<2)
-#  define VERT_UNIFORM_SCALE     ((Vert_gen_enum) 1<<3)
+/** Flags for generating Vertex shaders.
+ */
+# define Ver_gen_enum_SIZE          (sizeof(Vert_gen_enum))
+# define VER_EMPTY_SHADER           ((Ver_gen_enum) 0)
+# define VER_POSITION               ((Ver_gen_enum) 1)
+# define VER_VERTEX_COLORING        ((Ver_gen_enum) 1<<1)
+# define VER_VIEW_POSITION_UNI      ((Ver_gen_enum) 1<<2)
+# define VER_UNIFORM_SCALE          ((Ver_gen_enum) 1<<3)
 
-
-/** Flags for generating Tesselation Control shaders.
+/** Underlying type for Tesselation Control shader options enum.
  */
 typedef unsigned long long int TCS_gen_enum;
+/** Flags for generating Tesselation Control shaders.
+ */
+# define TCS_gen_enum_SIZE          (sizeof(TCS_gen_enum))
+# define TCS_EMPTY_SHADER           ((TCS_gen_enum) 0)
 
-/** Flags for generating Tesselation Evaluation shaders.
+/** Underlying type for Tesselation Evaluation shader options enum.
  */
 typedef unsigned long long int TEV_gen_enum;
+/** Flags for generating Tesselation Evaluation shaders.
+ */
+# define TEV_gen_enum_SIZE          (sizeof(TEV_gen_enum))
+# define TEV_EMPTY_SHADER           ((TEV_gen_enum) 0)
 
-/** Flags for generating Geometry shaders.
+/** Underlying type for Geometry shader options enum.
  */
 typedef unsigned long long int Geo_gen_enum;
+/** Flags for generating Geometry shaders.
+ */
+# define Geo_gen_enum_SIZE          (sizeof(Geo_gen_enum))
+# define GEO_EMPTY_SHADER           ((Geo_gen_enum) 0)
 
-/** Flags for generating Fragment shaders.
+/** Underlying type for Fragment shader options enum.
  */
 typedef unsigned long long int Fra_gen_enum;
+/** Flags for generating Fragment shaders.
+ */
+# define Fra_gen_enum_SIZE          (sizeof(Fra_gen_enum))
+# define FRA_EMPTY_SHADER           ((Fra_gen_enum) 0)
+
 #endif
