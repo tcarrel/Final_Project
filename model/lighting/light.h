ef9a564b380a6346a595ae7fc52c2aae766060c0
Thomas Russel Carrel <tcarrel@mail.csuchico.edu>
2017-04-21 22:20:02 -0700

    Began creating classes necessary for Blinn-Phong lighting model, to be used with deferred shading.

---
+++ model/lighting/light.h
@@ -0,0 +1,31 @@
+/**
+ *
+ * @file light.h
+ * @author Thomas R. Carrel
+ *
+ */
+
+
+
+
+#ifndef  _LIGHT_H_
+# define _LIGHT_H_
+
+namespace Model
+{
+    namespace Lighting
+    {
+        class Point_Light
+        {
+            public:
+
+            private:
+                glm::vec3 position_;
+                glm::vec3 color_;
+//                glm::vec3 brigh
+
+        }
+    } //Lighting namespace.
+} //Model namespace.
+
+#endif /* _LIGHT_H_ */
